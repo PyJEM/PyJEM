d8feca723ff7cfd694029e028e8ecdbf1b4b069c
DMaekawa <dmaekawa@jeol.co.jp>
2024-03-22 11:58:01 +0900

    docsの出力先の変更 - docs\html ⇒ docs
    Github Pagesからの参照も出来ていなかったため修正

--- src/make.bat
+++ src/make.bat
@@ -78,10 +78,12 @@ if errorlevel 9009 (
 
 
 if "%1" == "html" (
-	%SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%/html
+	rem default 
+	rem %SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%/html
+	%SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.Build finished. The HTML pages are in %BUILDDIR%/html.
+	echo.Build finished. The HTML pages are in %BUILDDIR%
 	goto end
 )
 
